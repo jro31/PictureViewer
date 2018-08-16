a0684deca9ee347ecc3f2f80a1ef776d5240d046
Jethro Williams <jethrowilliams4@hotmail.com>
2018-08-16 13:46:06 +0100

    Add PictureCell class and picture collection methods

---
+++ Challenge5/PictureCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class PictureCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var pictureTitle: UILabel!
+    
+}
--- Challenge5/ViewController.swift
+++ Challenge5/ViewController.swift
@@ -1,18 +1,33 @@
-//
-//  ViewController.swift
-//  Challenge5
-//
-//  Created by Jethro Williams on 16/08/2018.
-//  Copyright © 2018 Jethro Williams. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
+    
+    var pictures = [String]()
+    var numberOfPictures = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fileManager = FileManager.default
+        let picturePath = Bundle.main.resourcePath!
+        let picture = try! fileManager.contentsOfDirectory(atPath: picturePath)
+        
+        for eachPicture in picture {
+            if eachPicture.hasPrefix("nssl") {
+                pictures.append(eachPicture)
+            }
+        }
+        numberOfPictures = pictures.count
+        print(numberOfPictures)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfPictures
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Picture", for: indexPath) as! PictureCell
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
